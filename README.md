f134cce8842ed28cae836e32506bdf64a84fd988
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2024-12-20 15:32:41 +0100

    logica

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # js-mail-dadi
 21 Esercizio - dadi
+
+
+// Creo una lista utenti
+
+// propt per inserimento mail utente
+
+// verifico sia dentro la lista
+
+// SE la mail è presente
+//stampo messaggio di conferma
+
+
+//SE ALTRIMENTI non è presente
+
+
+//stampo messaggio di non conferma
---
+++ script/script.js
@@ -0,0 +1,22 @@
+// Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+// Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+// Si può fare? Certo che si basta ragionare un po’.
+// Nota: Non è necessario provvedere alla validazione delle email
+
+
+
+
+// Creo una lista utenti
+
+// propt per inserimento mail utente
+
+// verifico sia dentro la lista
+
+// SE la mail è presente
+    //stampo messaggio di conferma
+
+
+    //SE ALTRIMENTI non è presente
+
+
+    //stampo messaggio di non conferma
